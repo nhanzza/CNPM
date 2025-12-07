883c4a50774442ed747dc5e0ce6c552a0bf097db
TRAN QUOC HUY <ht0154043@gmail.com>
2025-12-07 20:55:18 +0700

    Update models.py

--- models.py
+++ models.py
@@ -34,3 +34,5 @@ class User(Base):
   is_active = Column(Boolean, default=True)
   created_at = Column(DateTime, default=datetime.utcnow)
   updated_at = Column(DateTime, default=datetime.utcnow, onupdate=datetime.utcnow)
+
+#
